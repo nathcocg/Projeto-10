383bbe2660b7a4342327fd3e3d25377d3b754e44
Nathália de Carvalho Oliveira Cunha Guimarães <96549422+nathcocg@users.noreply.github.com>
2021-12-23 13:47:23 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,38 @@
+var sea,ship;
+var seaImg,shipImg;
+
+function preload(){
+  seaImg = loadImage("sea.png");
+  shipImg1 = loadAnimation("ship-1.png","ship-1.png",
+                            "ship-2.png","ship-1.png");
+}
+
+function setup(){
+  createCanvas(400,400);
+  background("blue");
+
+  // Movendo o fundo
+  sea=createSprite(400,200);
+  sea.addImage(seaImg);
+  sea.velocityX = -5;
+  sea.scale=0.3;
+
+  
+  ship = createSprite(130,200,30,30);
+  ship.addAnimation("movingShip",shipImg1);
+  ship.scale =0.25;
+  
+}
+
+function draw() {
+  background(0);
+  sea.velocityX = -3;
+
+  
+  //código para redefinir o fundo
+  if(sea.x < 0){
+    sea.x = sea.width/8;
+  }
+    
+  drawSprites();
+}
